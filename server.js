fed0a7a96e2c871809f5be3d5f74fb3ca7ca5a58
cep98 <christoph.hillen@gmx.net>
2025-07-05 10:19:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// Statische Dateien aus dem "public"-Verzeichnis bereitstellen
 app.use(express.static("public"));
 
 io.on("connection", socket => {
-  // Zeichnen
+  // Zeichnen (z. B. für control/display)
   socket.on("draw", data => {
     socket.broadcast.emit("draw", data);
   });
 
-  // Alles löschen
+  // Löschen
   socket.on("clear", () => {
     io.emit("clear");
   });
 
-  // Sensor-Daten weiter
+  // Sensor-Daten für Debugging anzeigen
+  socket.on("sensorDump", data => {
+    io.emit("sensorDump", data);
+  });
+});
+
+http.listen(process.env.PORT || 3000, () => {
+  console.log("Server läuft");
+});
