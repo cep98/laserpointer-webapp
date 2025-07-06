0286973989f406d5911c39d0b954514c292c6c15
cep98 <christoph.hillen@gmx.net>
2025-07-06 19:19:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,31 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// Statische Dateien aus dem Ordner "public" bereitstellen
 app.use(express.static("public"));
 
 io.on("connection", socket => {
+  console.log("Ein Benutzer ist verbunden.");
+
   socket.on("draw", data => {
     socket.broadcast.emit("draw", data);
   });
+
+  socket.on("drawEnd", () => {
+    socket.broadcast.emit("drawEnd");
+  });
+
   socket.on("clear", () => {
     io.emit("clear");
   });
+
+  socket.on("disconnect", () => {
+    console.log("Ein Benutzer hat die Verbindung getrennt.");
+  });
 });
 
-http.listen(process.env.PORT || 3000, () => {
-  console.log("Server läuft");
+// Starte Server
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
 });
